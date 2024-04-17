5fd7ae1b8c2e423abaed305b7a06d05a9ce8268d
Lucas Lima <limalucks@hotmail.com>
2024-04-17 15:38:11 -0300

    Implementada a funcionalidade de se cadastrar como um dono de buffet!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
     protected 
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:is_buffet_owner])
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:is_buffet_owner, :name])
     end
 end
--- spec/system/user/user_sign_up_spec.rb
+++ spec/system/user/user_sign_up_spec.rb
@@ -1,22 +1,43 @@
 require 'rails_helper'
 
 describe 'Usuário se cadastra' do
+
     it 'com sucesso' do
         #Arrange
 
         #Act
         visit root_path
         click_on 'Cadastrar'
-        #fill_in 'Nome', with: 'Maria'
+        fill_in 'Nome', with: 'Maria'
         fill_in 'E-mail', with: 'maria@email.com'
         fill_in 'Senha', with: 'maria123'
         fill_in 'Confirme sua senha', with: 'maria123'
-        #selecionar "Dono de buffet"
         click_on 'Criar conta'
 
         #Assert
-        expect(current_path).to eq new_buffet_path
+        expect(page).to have_content 'Boas vindas! Você realizou seu registro com sucesso.'
+        expect(page).to have_content 'Maria'
+        expect(page).to have_content 'Sair'
 
+    end
+
+    it 'e é redirecionado para a página de cadastro de buffet' do
+        #Arrange
+
+        #Act
+        visit root_path
+        click_on 'Cadastrar'
+        fill_in 'Nome', with: 'Maria'
+        fill_in 'E-mail', with: 'maria@email.com'
+        fill_in 'Senha', with: 'maria123'
+        fill_in 'Confirme sua senha', with: 'maria123'
+        check "Possuo um Buffet."
+        click_on 'Criar conta'
+
+        #Assert
+        expect(current_path).to new_buffet_path
+        expect(page).to have_content 'Agora só falta cadastrar o seu buffet!'
+        expect(page).to have_content 'Sair'
     end
 
 end
